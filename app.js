c593d3020c67005dbce5663068c5f9b5f5deeee7
nbkhope <ryumemaru@yahoo.com>
2016-07-12 19:22:41 -0700

    Add NotesCtrl and notes view; move todos variable of fake data into a service and provide servide CRUD methods through that

--- app.js
+++ app.js
@@ -9,10 +9,16 @@ angular.module('todoList', ['ui.router'])
         controller: 'TodosCtrl',
         controllerAs: 'ctrl',
       })
+      .state('notes', {
+        url: '/todos/:id/notes',
+        templateUrl: 'notes.html',
+        controller: 'NotesCtrl',
+        controllerAs: 'ctrl',
+      })
       ;
   })
 
-  .controller('TodosCtrl', function() {
+  .controller('TodosCtrl', ['TodosModel', function(TodosModel) {
     var ctrl = this;
 
     ctrl.newTodo = {
@@ -20,7 +26,8 @@ angular.module('todoList', ['ui.router'])
     };
 
     ctrl.addTodo = function(todo) {
-      ctrl.todos.push(todo);
+      TodosModel.create(todo);
+      ctrl.getTodos();
 
       ctrl.newTodo = {
         content: ""
@@ -28,23 +35,65 @@ angular.module('todoList', ['ui.router'])
     };
 
     ctrl.deleteTodo = function(todoId) {
-      for (var index in ctrl.todos) {
-        if (ctrl.todos[index].id === todoId) {
-          ctrl.todos.splice(index, 1);
+      TodosModel.destroy(todoId);
+      ctrl.getTodos();
+    };
+
+    ctrl.getTodos = function() {
+      ctrl.todos = TodosModel.all();
+    };
+
+    ctrl.getTodos();
+  }])
+
+  .controller('NotesCtrl', ['$stateParams', '$state', 'TodosModel', function($stateParams, $state, TodosModel) {
+    var ctrl = this;
+
+    ctrl.getTodo = function(todoId) {
+      ctrl.todo = TodosModel.fetch(todoId);
+    }
+
+    ctrl.deleteTodo = function(todoId) {
+      TodosModel.destroy(todoId);
+      $state.go('todos');
+    };
+
+    ctrl.getTodo($stateParams.id);
+  }])
+  .service('TodosModel', function() {
+    var service = this;
+
+    // Fake Data
+    var todos = [
+      { id: 1, content: "Do the dishes", complete: false },
+      { id: 2, content: "Clean my room", complete: true },
+      { id: 3, content: "Walk the dog", complete: true },
+      { id: 4, content: "Do the laundry", complete: false },
+    ];
+
+    service.all = function() {
+      return todos;
+    };
+
+    service.fetch = function(todoId) {
+      return todos.filter(function(item) { return item.id == todoId })[0];
+    };
+
+    service.create = function(todo) {
+      todo.id = todos[todos.length - 1].id + 1;
+      todo.complete = false;
+
+      todos.push(todo);
+    };
+
+    service.destroy = function(todoId) {
+      for (var index in todos) {
+        if (todos[index].id === todoId) {
+          todos.splice(index, 1);
           return;
         }
       }
     };
 
-    ctrl.getTodos = function() {
-      ctrl.todos = [
-        { id: 1, content: "Do the dishes", complete: false },
-        { id: 2, content: "Clean my room", complete: true },
-        { id: 3, content: "Walk the dog", complete: true },
-        { id: 4, content: "Do the laundry", complete: false },
-      ];
-    };
-
-    ctrl.getTodos();
   })
   ;
