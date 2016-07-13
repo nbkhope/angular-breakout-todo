c0a231b52fad055f036ab392c3c25ae95a1a8c92
nbkhope <ryumemaru@yahoo.com>
2016-07-12 18:08:34 -0700

    Add basic todo list page with fake data served from controller

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ angular.module('todoList', ['ui.router'])
       .state('todos', {
         url: '/todos',
         templateUrl: 'todos.html',
+        controller: 'TodosCtrl',
+        controllerAs: 'ctrl',
       })
       ;
-  });
+  })
+
+  .controller('TodosCtrl', function() {
+    var ctrl = this;
+
+    ctrl.getTodos = function() {
+      ctrl.todos = [
+        { content: "Do the dishes", complete: false },
+        { content: "Clean my room", complete: true },
+        { content: "Walk the dog", complete: true },
+        { content: "Do the laundry", complete: false },
+      ];
+    };
+
+    ctrl.getTodos();
+  })
+  ;
