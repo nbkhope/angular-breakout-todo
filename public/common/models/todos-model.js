f62fb4dc78dc1a1e04e30b8f7eea60c65eecedcf
nbkhope <ryumemaru@yahoo.com>
2016-07-13 22:20:37 -0700

    Indent .service block

--- public/common/models/todos-model.js
+++ public/common/models/todos-model.js
@@ -1,25 +1,25 @@
 angular.module('todoList')
-.service('TodosModel', ['$http', 'ENDPOINT_URI', function($http, ENDPOINT_URI) {
-  var service = this;
+  .service('TodosModel', ['$http', 'ENDPOINT_URI', function($http, ENDPOINT_URI) {
+    var service = this;
 
-  service.all = function() {
-    return $http.get(ENDPOINT_URI + "todos");
-  };
+    service.all = function() {
+      return $http.get(ENDPOINT_URI + "todos");
+    };
 
-  service.fetch = function(todoId) {
-    return $http.get(ENDPOINT_URI + "todos/" + todoId);
-  };
+    service.fetch = function(todoId) {
+      return $http.get(ENDPOINT_URI + "todos/" + todoId);
+    };
 
-  service.create = function(todo) {
-    return $http.post(ENDPOINT_URI + "todos", todo);
-  };
+    service.create = function(todo) {
+      return $http.post(ENDPOINT_URI + "todos", todo);
+    };
 
-  service.update = function(todoId, todo) {
-    return $http.put(ENDPOINT_URI + "todos/" + todoId, todo);
-  };
+    service.update = function(todoId, todo) {
+      return $http.put(ENDPOINT_URI + "todos/" + todoId, todo);
+    };
 
-  service.destroy = function(todoId) {
-    return $http.delete(ENDPOINT_URI + "todos/" + todoId);
-  };
+    service.destroy = function(todoId) {
+      return $http.delete(ENDPOINT_URI + "todos/" + todoId);
+    };
 
-}]);
+  }]);
