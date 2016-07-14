7fb7016e3ac0b34af2eb067210807ebbd609197d
nbkhope <ryumemaru@yahoo.com>
2016-07-13 22:21:55 -0700

    Move NotesCtrl into its own file

--- public/app.js
+++ public/app.js
@@ -75,39 +75,4 @@ angular.module('todoList', ['ui.router'])
     // Retrieve all the todos the first time the view is loaded
     ctrl.getTodos();
   }])
-
-  .controller('NotesCtrl', ['$stateParams', '$state', 'TodosModel', function($stateParams, $state, TodosModel) {
-    var ctrl = this;
-
-    ctrl.getTodo = function(todoId) {
-      TodosModel.fetch(todoId)
-        .then(function(response) {
-          ctrl.todo = response.data;
-        });
-    }
-
-    ctrl.updateTodo = function(todo) {
-      TodosModel.update(todo.id, todo)
-        .then(function(response) {
-          // do nothing
-        })
-        .catch(function(error) {
-          console.log("Failed to update todo #" + todo.id);
-        });
-    };
-
-    ctrl.deleteTodo = function(todoId) {
-      TodosModel.destroy(todoId)
-        .then(function(response) {
-          // Go back to the list of todos after deleting this specific one
-          // You can use $state.go to go to a specific state (aka route)
-          // Besides this, using the attribute ui-sref in <a> tags also
-          // does the same thing
-          $state.go('todos');
-        });
-    };
-
-    // Retrieve the todo item the first time the controller is loaded
-    ctrl.getTodo($stateParams.id);
-  }])
   ;
